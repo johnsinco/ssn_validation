840490179861934048bf477a00fbaebaf1bf5b07
John Stewart (johnsinco) <john.stewart@experian.com>
2019-10-09 09:16:33 -0600

    make activesupport optional, bump version

--- lib/ssn_validation.rb
+++ lib/ssn_validation.rb
@@ -1,3 +1,9 @@
 require 'ssn_validation/ssn_validation'
 require 'ssn_validation/ssn'
-require 'validators/social_security_number_validator'
+
+# optional ActiveModel dependency for custom validator
+begin
+  require 'validators/social_security_number_validator'
+rescue 
+  nil
+end
